af7efd36e30dcfdb0babc1596c9aaaec7d816253
myOnlineCourses <160550834+myOnlineCourses@users.noreply.github.com>
2024-02-19 20:42:27 -0500

    Add files via upload

---
+++ day01/01_variables.js
@@ -0,0 +1,30 @@
+//Assignments
+// **** day 1 ***
+/*
+Assignment:1
+------------
+ Javascript File Name: 01_learn.js
+ 
+    Objective: Learn Primitive Data Types
+ 
+    Create the following variables using var and check their typeOf
+ 
+    a) firstName
+    b) companyName
+    c) mobileNumber
+    d) isAutomation
+    e) hasPlaywright (do not assign)
+ 
+    print and confirm the values and data types
+ */
+    var firsName = "Jassan"
+    var companyName = "apple"
+    var mobileNumber = 1234567890
+    var isAutomation = true;
+    var hasPlaywright
+    
+    console.log(typeof firsName);
+    console.log(typeof companyName);
+    console.log(typeof mobileNumber);
+    console.log("isAutomation is of type : ", typeof isAutomation);
+    console.log("hasPlaywright is of type : ", typeof hasPlaywright);
